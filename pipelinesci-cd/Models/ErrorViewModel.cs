b02d97b150c13e5a849f1b2c30c535bd9d31daba
Sinako Brian Mzotsho <sinakobrian3@gmail.com>
2024-10-29 13:39:40 +0200

    Add project files.

---
+++ pipelinesci-cd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pipelinesci_cd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
